162ddb98382131895bc70b1b2121f6bb9345fdf3
Can Arel <can@campanja.com>
2013-05-21 17:57:29 +0200

    Removing poolboy deps

--- apps/e_ann/src/e_ann_handler.erl
+++ apps/e_ann/src/e_ann_handler.erl
@@ -2,9 +2,8 @@
 %%% @author cantheman <can@campanja.com>
 %%% @copyright (C) 2013, cantheman
 %%% @doc
-%%% This module reads all the inputs and divides them among the pool
-%%% processes. Should more processes be needed it spawns more before
-%%% it starts training on input data.
+%%% This module reads the architecture config and lets the supervisors spawn 
+%%% child processes accordingly.
 %%% @end
 %%% Created :  19 May 2013 by cantheman <can@campanja.com>
 %%%-------------------------------------------------------------------
